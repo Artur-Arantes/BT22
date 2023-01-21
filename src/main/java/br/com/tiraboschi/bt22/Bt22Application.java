70cb546ab4378082e94a5b2ddb1b316bdf0d0208
artur.tiraboschi <artur.tiraboschi@socin.com.br>
2023-01-20 21:38:27 -0300

    feat:commit initial

---
+++ src/main/java/br/com/tiraboschi/bt22/Bt22Application.java
@@ -0,0 +1,13 @@
+package br.com.tiraboschi.bt22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bt22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bt22Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tiraboschi/bt22/Bt22ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tiraboschi.bt22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bt22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
