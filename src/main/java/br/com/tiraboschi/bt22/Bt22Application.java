3a03d770c8fe8a9bd28d0196cb80277384c0f757
artur.tiraboschi <artur.tiraboschi@socin.com.br>
2023-01-22 13:02:41 -0300

    feat: -data initial fix: -Error because of the missed annotation on BT222App.java test: -Test for TokenHelper

--- src/main/java/br/com/tiraboschi/bt22/Bt22Application.java
+++ src/main/java/br/com/tiraboschi/bt22/Bt22Application.java
@@ -1,8 +1,11 @@
 package br.com.tiraboschi.bt22;
 
+import br.com.tiraboschi.bt22.config.ConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(ConfigProperties.class)
 @SpringBootApplication
 public class Bt22Application {
 
