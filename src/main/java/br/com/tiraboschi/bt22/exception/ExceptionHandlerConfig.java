971c31881d927f2a7c9621a7d917246883773394
artur.tiraboschi <artur.tiraboschi@socin.com.br>
2023-01-26 20:50:40 -0300

    feat: -ExceptionHandler.class

---
+++ src/main/java/br/com/tiraboschi/bt22/exception/ExceptionHandlerConfig.java
@@ -0,0 +1,11 @@
+package br.com.tiraboschi.bt22.exception;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Configuration
+public class ExceptionHandlerConfig {
+
+
+}
